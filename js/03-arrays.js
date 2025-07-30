2a9a94d95f8fbd74947096c00cfca9c7e3726171
Kenny <shadow.dio.shadow@gmail.com>
2025-07-30 17:28:30 -0300

    Update 03-arrays.js

--- js/03-arrays.js
+++ js/03-arrays.js
@@ -1,3 +1,13 @@
 'use strict';
 
-console.log("teste");
+// Declarando um array (vetor)
+let guitarristas = ["Dimebag" ,"Kirk", "Marty", "Wes"];
+
+// Se quisermos visualizar a estrutura do array inteiro:
+// console.log(guitarristas[0]);
+
+// Acessando um determinado elemento através do índice
+console.log(guitarristas);
+
+// O Kirk está em turnê tocando Metallica
+console.log(`O ${guitarristas[1]} está em turnê tocando Metallica`);

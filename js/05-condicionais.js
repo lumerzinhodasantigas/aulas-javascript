67afbe59eee24d27602e878ae0f2450b3050b990
Kenny <shadow.dio.shadow@gmail.com>
2025-08-19 14:56:12 -0300

    Update 05-condicionais.js

--- js/05-condicionais.js
+++ js/05-condicionais.js
@@ -13,3 +13,15 @@ let numero = 50;
 if(numero >= 10){
     console.log("Se você está lendo essa mensagem, é porque a condição é VERDADEIRA!");
 }
+
+console.log("\nExemplo 2");
+
+let aluno = "Bob";
+let idade = 18;
+
+/* Lógia: verificar se o aluno é maior ou menor de idade. */
+if(idade < 18){
+    console.log("Você é menor de idade!");
+} else{
+    console.log("Você é maior de idade!");
+}

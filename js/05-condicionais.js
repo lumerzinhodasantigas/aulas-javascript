2013485bceb78244df56f23914aeed6a7c7b13ab
Kenny <shadow.dio.shadow@gmail.com>
2025-08-19 14:36:19 -0300

    Condicionais Simples

---
+++ js/05-condicionais.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/* Comandos Condicionais mais comuns 
+if -> Se
+else - SE NÃO
+*/
+
+console.log("Exemplo 1");
+
+let numero = 50;
+
+// Condicional SIMPLES (usa apenas if)
+if(numero >= 10){
+    console.log("Se você está lendo essa mensagem, é porque a condição é VERDADEIRA!");
+}

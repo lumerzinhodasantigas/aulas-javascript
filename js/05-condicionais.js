53653f0ce2e79e7e39369f5baf4103a391c60b5a
Kenny <shadow.dio.shadow@gmail.com>
2025-08-19 16:52:27 -0300

    Operadores lógicos

--- js/05-condicionais.js
+++ js/05-condicionais.js
@@ -10,7 +10,7 @@ console.log("Exemplo 1");
 let numero = 50;
 
 // Condicional SIMPLES (usa apenas if)
-if(numero >= 10){
+if (numero >= 10) {
     console.log("Se você está lendo essa mensagem, é porque a condição é VERDADEIRA!");
 }
 
@@ -20,8 +20,65 @@ let aluno = "Bob";
 let idade = 18;
 
 /* Lógia: verificar se o aluno é maior ou menor de idade. */
-if(idade < 18){
+if (idade < 18) {
     console.log("Você é menor de idade!");
-} else{
+} else {
     console.log("Você é maior de idade!");
 }
+
+// Exercício Condicional
+
+/* 1. Crie duas variáveis conforme a seguir:
+ 
+- Nota 1 (contendo um valor de 0 a 10)
+- Nota 2 (contendo outro valor de 0 a 10)*/
+
+let Nota1 = 9;
+
+let Nota2 = 10;
+
+/* 2. Crie uma variável chamada "Média" que receberá o valor CALCULADO da média das duas notas informadas. 
+DICA: você deve SOMAR as duas notas e DEPOIS dividir por 2. */
+
+let media = (Nota1 + Nota2) / 2;
+
+/* 3. Programe uma condicional que verifique o valor da média calculada. Se a média for maior/igual a 7, mostre "aprovado". 
+Caso contrário, mostre "reprovado". */
+
+if (media >= 7) {
+    console.log("Aprovado!");
+} else {
+    console.log("Reprovado!");
+}
+
+// Condicionada E
+
+/* Verificando o desempenho do aluno conforme a média:
+ - Média acima de 9? Ótimo
+ - Média acima de 7? Bom
+ - Média acima de 5? Ruim
+ - Média até 5? Péssimo */
+
+if (media > 9) {
+    console.log("ÓTIMO!");
+} else if (media > 7) {
+    console.log("BOM!");
+} else if (media > 5) {
+    console.log("RUIM!");
+} else {    
+    console.log("PÉSSIMO!");
+}
+
+console.log("\nExemplo 4");
+
+if(idade >= 18){
+    console.log("maior");
+} else{
+    console.log("menor");
+}
+
+// OU, pode ser feito da seguinte forma...:
+
+let situacao = idade >= 18 ? "maior" : "menor";
+
+console.log(situacao)
---
+++ js/06-operadores-lógicos.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/* Operadores Lógicos
+
+&& -> E -> AND
+|| -> OU -> OR
+! -> NÃO -> NOT
+*/
+
+// && -> retorna TRUE se TODAS as condições forem VERDADEIRAS
+console.log("Exemplo &&");
+let idade = 20;
+let temCarta = true;
+
+if(idade >= 18 && temCarta === true){
+    console.log("Pode dirigir");
+} else{
+    console.log("Não pode dirigir");
+}
+
+/* Sobre duplo e triplo sinal de igual
+ - Ao usar ==  a comparação é somente do VALOR, ou seja, NÃO IMPORTA o tipo de dados (string, number, etc) 
+ 
+ - Ao usar === a comparação é do VALOR E DO TIPO DE DADO, ou seja, IMPORTA TUDO. */
+ 
+let a = 10;
+let b = 10;
+// let resultadoComparacao = a ==b; // TRUE
+let resultadoComparacao = a === b; // FALSE
+
+console.log(resultadoComparacao);
